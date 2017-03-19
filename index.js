8c87ab0c3a2d4a131ae39b65404d9ba55fab9db2
dison ruan <dison.ruan@yahoo.com>
2017-03-19 18:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+function Sandwich(bread, ingredients, name){
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+function serve(){
+  if(arguments.length > 0){
+    var customer = Array.prototype.slice.call(arguments);
+    last = customer.pop;
+    console.log(this.name + " for " + customer.join(", ") + " and" + last + ". Enjoy!");
+  }else {
+      console.log(this.name + ". Order up!");
+  }
+}
+
+let pbj = new Sandwich("white", ["peanut-butter", "jelly"], "Peanut Butter and Jelly");
